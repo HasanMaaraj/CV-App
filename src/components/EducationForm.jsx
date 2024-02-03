2a366bc97937dad4f53f2b5204e9ba5ecf56e1ef
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-02-03 18:36:01 +0300

    Create educational experience form

---
+++ src/components/EducationForm.jsx
@@ -0,0 +1,19 @@
+
+
+
+function EducationForm(props) {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <legend>Educational Experience</legend>
+            <label htmlFor="school-name">School Name: </label>
+            <input type="text" name="school-name" id="school-name"/>
+            <label htmlFor="study-title">Title of Study: </label>
+            <input type="text" name="study-title" id="study-title"/>
+            <label htmlFor="graduation-date">Graduation Date: </label>
+            <input type="date" name="graduation-date" id="graduation-date"/>
+            <button type="submit">submit</button>
+        </form>
+    )
+    }
+    
+    export default EducationForm;
