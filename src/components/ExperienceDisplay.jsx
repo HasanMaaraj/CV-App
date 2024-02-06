6aaacf8df1a973ef757d561171bcebf0fd4aec6b
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-02-06 19:49:34 +0300

    Edit the properties of the experience display

--- src/components/ExperienceDisplay.jsx
+++ src/components/ExperienceDisplay.jsx
@@ -5,11 +5,11 @@ function ExperienceDisplay(props) {
     return (
         <div className="experience-display">
             <h2>Experience</h2>
-            <div className="company">Company: {props.company}</div>
-            <div className="title">Position Title: { props.title }</div>
-            <div className="start-Date">Start Date { props.startDate }</div>
-            <div className="end-Date">End Date { props.endDate }</div>
-            <div className="description">Description: { props.description }</div>
+            <div className="company">Company: {props.info['company-name']}</div>
+            <div className="title">Position Title: { props.info['position-title'] }</div>
+            <div className="start-Date">Start Date { props.info['starting-date'] }</div>
+            <div className="end-Date">End Date { props.info['end-date'] }</div>
+            <div className="description">Description: { props.info.description }</div>
         </div>
     )
 }
