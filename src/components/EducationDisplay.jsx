4e699b55d6f1904973b3fbbd4883b2fa9ce9be04
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-02-06 19:49:17 +0300

    Edit the properties of the education display

--- src/components/EducationDisplay.jsx
+++ src/components/EducationDisplay.jsx
@@ -5,9 +5,10 @@ function EducationDisplay(props) {
     return (
         <div className="education-display">
             <h2>Education</h2>
-            <div className="degree">Degree: {props.degree}</div>
-            <div className="title">Title of Study: { props.title }</div>
-            <div className="graduation-date">Graduation Date { props.date }</div>
+            <div className="school">School: {props.info['school-name']}</div>
+            <div className="degree">Degree: {props.info.degree}</div>
+            <div className="title">Title of Study: { props.info['study-title'] }</div>
+            <div className="graduation-date">Graduation Date: { props.info['graduation-date'] }</div>
         </div>
     )
 }
