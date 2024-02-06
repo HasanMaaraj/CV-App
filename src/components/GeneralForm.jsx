605c5a846c384a4d9ab154279117d134e542512f
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-02-06 19:41:34 +0300

    Edit general form's properties

--- src/components/GeneralForm.jsx
+++ src/components/GeneralForm.jsx
@@ -3,12 +3,12 @@
 
 function GeneralForm(props) {
 return (
-    <form onSubmit={props.handleSubmit}>
+    <form onSubmit={props.onSubmit} id="general-form">
         <legend>General Information</legend>
         <label htmlFor="name">Name: </label>
         <input type="text" name="name" id="name"/>
-        <label htmlFor="e-mail">E-Mail: </label>
-        <input type="email" name="e-mail" id="e-mail"/>
+        <label htmlFor="email">E-Mail: </label>
+        <input type="email" name="email" id="email"/>
         <label htmlFor="phone">Phone Number: </label>
         <input type="tel" name="phone" id="phone"/>
         <button type="submit">submit</button>
